e914df6cde605b917a87d722d3e3292f0fa40a69
Alex Maya <Alex Maya@DESKTOP-DSFHD27>
2023-02-28 16:12:14 -0500

    Agregar archivos de proyecto.

---
+++ ConceptsPOO/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConceptsPOO;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+
+try
+{
+    Console.WriteLine(new Date(2023, 02, 28));
+    Console.WriteLine(new Date(1983, 03, 21));
+    Console.WriteLine(new Date(1733, 11, 39));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
+
